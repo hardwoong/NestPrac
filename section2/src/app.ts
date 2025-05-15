ae40afd30db70a0a20663542c0e812d2d0f9cf0f
hardwoong <dew04300@naver.com>
2025-05-15 15:50:29 +0900

    half

--- section2/src/app.ts
+++ section2/src/app.ts
@@ -1,39 +1,25 @@
 import * as express from 'express';
-import { Cat, CatType } from './app.model';
+import catsRouter from './cats/cats.route';
 
 const app: express.Express = express();
 
+// logging middleware
 app.use((req, res, next) => {
   console.log(req.headers['user-agent']);
-  console.log('this is logging middleware')
+  console.log('this is logging middleware');
   next();
 });
 
+// json middleware
+app.use(express.json());
 
-app.get('/cats/som', (req, res, next) => {
-  console.log(req.headers['user-agent']);
-  console.log('this is som middleware')
-  next();
-});
+app.use(catsRouter);
 
-const data = [1, 2, 3, 4];
-
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
-});
-
-app.get("/cats/blue", (req, res, next: express.NextFunction) => {
-  res.send({ blue: Cat[0] });
-});
-
-app.get("/cats/som", (req, res) => {
-  res.send({ som: Cat[1] });
-});
-
+// 404 middleware
 app.use((req, res, next) => {
   console.log('this is error middleware');
   res.send({ error: '404 not found error' });
-})
+});
 
 app.listen(8000, () => {
   console.log('server is on...');
